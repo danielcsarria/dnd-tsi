07f949b649d16c50b68fb1ac4e111d5b6910d2ab
Juan F Sarria <juan@townsquaredigital.com>
2019-04-22 18:07:32 -0400

    Fixing the code: State, Lifting up. Its working now

--- src/RightSide/ColumnRight.js
+++ src/RightSide/ColumnRight.js
@@ -1,108 +1,22 @@
 import React from 'react';
 import ModuleRight from './ModuleRight';
-import data from '../data/data.json';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import '../style.css';
 
 export default class ColumnRight extends React.Component{
 
-    state = data;    
+    constructor(props) {
+        super(props);
+        this.onDragEnd = this.onDragEnd.bind(this);
+    }  
 
-    onDragEnd_R = result => {
-
-        const columnIndex = this.props.columnIndex;
-
-        const { destination, source } = result;
-
-        if (!destination) {
-            return;            
-        }
-
-        if (
-            destination.droppableId === source.droppableId &&
-            destination.index === source.index
-        ) {
-            return;
-        }
-
-        var source_module = parseInt(source.droppableId, 10),
-            target_module = parseInt(destination.droppableId, 10),
-            source_item_index = source.index,
-            target_item_index = destination.index;
-
-        // console.log("A columnIndex => ", columnIndex);
-        // console.log("source_module => ", source_module);
-        // console.log("target_module => ", target_module);
-        // console.log("source_item_index => ", source_item_index);
-        // console.log("target_item_index => ", target_item_index);      
-
-        if(isNaN(source_module)) {
-            
-            const fillOutModules = function(keys, allModules) {
-                // console.log("-> ", source_column, target_column);
-                var newModules = {};
-                keys.forEach(key => {
-                    console.log("B columnIndex => ", columnIndex);
-                    console.log(key)
-                    newModules[key] = allModules[key];
-                });
-                // console.log("New Modules", newModules);
-                return newModules;
-            }            
-
-            var modules = this.state.modules[columnIndex],
-                keys = Object.keys(modules);
-            
-            // console.log("keys =>", keys);
-
-            var modSelected = keys.splice(source_item_index, 1);
-            keys.splice(target_item_index, 0, modSelected[0]);
-
-            // const newModules = Array.from(keys);
-            // const modSelected = newModules.splice(source.index, 1);
-            // newModules.splice(destination.index, 0, modSelected[0]);
-
-
-            // console.log("modSelected =>", modSelected)
-            // console.log("keys =>", keys);
-
-            modules = fillOutModules(keys, modules);
-
-            // this.setState(newModules);
-
-        } else {
-
-            var modsKeys = Object.keys(this.state.modules[columnIndex]),
-            source_module_key = modsKeys[source_module],
-            target_module_key = modsKeys[target_module],
-            source_items = this.state.modules[columnIndex][source_module_key].items,
-            target_items = this.state.modules[columnIndex][target_module_key].items;
-
-            if (source_module === target_module) {
-
-                var selected = source_items.splice(source_item_index, 1);
-                source_items.splice(target_item_index, 0, selected[0]);
-    
-            } else {
-    
-                var selectedB = source_items.splice(source_item_index, 1);
-                target_items.splice(target_item_index, 0, selectedB[0]);
-    
-            }
-
-            // source_items = [];
-        }
-        
-        
-
-        this.setState(data);
-        
+    onDragEnd(result) {
+        this.props.onDragEnd(result, this.props.columnIndex);
     }
 
     render(){
         const columnIndex = this.props.columnIndex;
-        // const modules = this.props.modules;
-        const modules = this.state.modules[columnIndex];
+        const modules = this.props.modules[columnIndex];
         var title = columnIndex === 0 ? "Feature" : "Column " + columnIndex;
 
         var background = "column-" + columnIndex;
@@ -115,7 +29,7 @@ export default class ColumnRight extends React.Component{
                 <div className="column-title">
                     {title}
                 </div>
-                <DragDropContext onDragEnd={this.onDragEnd_R}>
+                <DragDropContext onDragEnd={this.onDragEnd}>
                     <Droppable droppableId="all-modules" type="modules" direction="vertical">
                         {(provided) => (
                             <div className="container"
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import data from './data/data.json';
+import initialData from './data/data.json';
 import config from './data/config.json'
 import ColumnLeft from './LeftSide/ColumnLeft';
 import ColumnRight from './RightSide/ColumnRight';
@@ -9,10 +9,14 @@ import './style.css';
 
 class LayOut extends React.Component {
 
-    state = data;
+    constructor(props) {
+        super(props);
+        this.state = {data: initialData};
+        this.onDragEnd_L = this.onDragEnd_L.bind(this);
+        this.onDragEnd_R = this.onDragEnd_R.bind(this);
+    }  
 
-
-    onDragEnd_L = result => {
+    onDragEnd_L(result) {
 
         const { destination, source, draggableId } = result;
 
@@ -45,41 +49,53 @@ class LayOut extends React.Component {
         // console.log("target_module_index => ", target_module_index);
         // console.log("source_module_id => ", source_module_id);
 
-        const fillOutModules = function(keys) {
-            // console.log("-> ", source_column, target_column);
-            var modules = {};
-            keys.forEach(key => {
-                modules[key] = typeof data.modules[source_column][key] != "undefined" ? 
-                                data.modules[source_column][key] : 
-                                data.modules[target_column][key];
-            });
-            return modules;
-        }
-
         // console.log("Modules", modules)
 
         // console.log("target_module_id", target_module_id);
         // console.log("source modules Left => ", data.modules[source_column]);
 
+        const fillOutModules = (keys) => {
+            // console.log("-> ", source_column, target_column);
+            var modules = {};
+            keys.forEach(key => {
+                modules[key] = typeof this.state.data.modules[source_column][key] != "undefined" ? 
+                                this.state.data.modules[source_column][key] : 
+                                this.state.data.modules[target_column][key];
+            });
+            return modules;
+        }        
         
 
         if(isNaN(source_column)) {
-            
 
             if (source_column !== target_column) {
-                var columns  = this.state.modules;
+                console.log("A")
+                var columns = this.state.data.modules;
                 console.log("columns =>", columns);
 
                 var selected = columns.splice(source.index, 1);
                 columns.splice(destination.index, 0, selected[0]);
                 console.log("source index =>", source.index);
                 console.log("destination index => ", destination.index);
+
+                const newData = {
+                    ...this.state.data,
+                    modules: columns
+                }
+        
+                this.setState({
+                    data : newData
+                });          
             }
 
         } else {
+            const newData = this.state.data;
+            const modules = newData.modules;
 
             if (source_column === target_column) {
-                var keys = Object.keys(data.modules[source_column]);
+                console.log("B")
+
+                const keys = Object.keys(modules[source_column]);
     
                 if (target_module_index > source_module_index) {
                     ++target_module_index;
@@ -90,34 +106,133 @@ class LayOut extends React.Component {
                 keys.splice(target_module_index, 0, source_module_id);
                 keys.splice(source_module_index, 1)
     
-                var modules = fillOutModules(keys);
-                data.modules[source_column] = modules;
+                newData.modules[source_column] = fillOutModules(keys);
     
                 // console.log("keys", keys);
                 // console.log("modules", modules);
-    
-                // this.setState(data.modules[source_column]);
+                // console.log("newData", newData);
+               
     
             } else {
-                var source_keys = Object.keys(data.modules[source_column]),
-                    target_keys = Object.keys(data.modules[target_column]);
+                console.log("C")
+
+                var source_keys = Object.keys(modules[source_column]),
+                    target_keys = Object.keys(modules[target_column]);
     
                 target_keys.splice(target_module_index, 0, source_module_id);
-                data.modules[target_column] = fillOutModules(target_keys);
+                newData.modules[target_column] = fillOutModules(target_keys);
                 // this.setState(data.modules[target_column]);
     
                 source_keys.splice(source_module_index, 1);
-                data.modules[source_column] = fillOutModules(source_keys);
-                // this.setState(data.modules[source_column]);
+                newData.modules[source_column] = fillOutModules(source_keys);
+            
+            }
+
+            this.setState({
+                data : newData
+            });    
+        }
+        
+    }
+
+    onDragEnd_R(result, columnIndex) {
+
+        const { destination, source, draggableId } = result;
+
+        if (!destination) {
+            return;            
+        }
+
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+        // const columnIndex = this.props.columnIndex;
+        console.log("A columnIndex => ", columnIndex);
+
+        var source_module = parseInt(source.droppableId, 10),
+            target_module = parseInt(destination.droppableId, 10),
+            source_item_index = source.index,
+            target_item_index = destination.index;
+
+        console.log("source_module => ", source_module);
+        console.log("target_module => ", target_module);
+        console.log("source_item_index => ", source_item_index);
+        console.log("target_item_index => ", target_item_index);      
+        console.log("draggableId =>", draggableId);
+
+        const newData = this.state.data;
+
+        if(isNaN(source_module)) {
+
+            console.log("R A")
+            
+            const fillOutModules = (keys, column) => {
+                var modules = [];
+                keys.forEach(key => {
+                    modules[key] = column[key];
+                });
+                return modules;
+            }            
+
+            const modules = newData.modules;
+            const keys = Object.keys(modules[columnIndex]);
+            const source_module_id = draggableId;
+    
+            if (target_item_index > source_item_index) {
+                ++target_item_index;
+            } else {
+                ++source_item_index;
+            }
+
+            keys.splice(target_item_index, 0, keys[source_module_id]);
+            keys.splice(source_item_index, 1);
+
+            // console.log("keys",keys);
+
+            const newModules = fillOutModules(keys, this.state.data.modules[columnIndex]);
+            // console.log("newModules",newModules);
+
+            newData.modules[columnIndex] = newModules;
+
+        } else {
+
+            console.log("R B")
+            
+            const modules = newData.modules;   
+
+            var modsKeys = Object.keys(modules[columnIndex]),
+                source_module_key = modsKeys[source_module],
+                target_module_key = modsKeys[target_module],
+                source_items = modules[columnIndex][source_module_key].items,
+                target_items = modules[columnIndex][target_module_key].items;
+
+            if (source_module === target_module) {
+
+                var selected = source_items.splice(source_item_index, 1);
+                source_items.splice(target_item_index, 0, selected[0]);
+                newData.modules[columnIndex][source_module_key].items = source_items;
+    
+            } else {
+    
+                var selectedB = source_items.splice(source_item_index, 1);
+                target_items.splice(target_item_index, 0, selectedB[0]);
+                newData.modules[columnIndex][target_module_key].items = target_items;
     
             }
 
-
         }
 
-        this.setState(data);
+        // console.log("newData", newData)        
         
-    }
+        this.setState({
+            data : newData
+        });    
+        
+    }    
 
     render(){
 
@@ -148,12 +263,11 @@ class LayOut extends React.Component {
                                     ref = {provided.innerRef}
                                 >
                                     {   
-                                        this.state.modules.map((modules, columnIndex) => {
+                                        this.state.data.modules.map((modules, columnIndex) => {
                                             return <ColumnLeft 
                                                 key={columnIndex} 
                                                 columnIndex={columnIndex} 
                                                 modules={modules} 
-                                                data={data} 
                                                 config={config} 
                                             />
                                         })
@@ -167,13 +281,13 @@ class LayOut extends React.Component {
 
                     <div id="column-container-right">
                         {
-                            this.state.modules.map((modules, columnIndex) => {
+                            this.state.data.modules.map((modules, columnIndex) => {
                                 return <ColumnRight 
                                     key={columnIndex} 
                                     columnIndex={columnIndex} 
-                                    modules={modules} 
-                                    state={this.state}
+                                    modules={this.state.data.modules} 
                                     config={config}
+                                    onDragEnd={this.onDragEnd_R}
                                 />
                             })
                         }
